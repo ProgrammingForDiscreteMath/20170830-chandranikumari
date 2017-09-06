7337f23ed5e9e8b28a9f2368a3b9a7ce9ecb82e7
chandranikumari <chandraniku@gmail.com>
2017-09-06 22:43:56 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,14 +1,16 @@
 # 1
 # Replace if-else with try-except in the the example below:
 
-def fourth_element_of_list(L):
+def fourth_element_of_list(L,i):
     """
     Return the nth element of ``L`` if it exists, ``None`` otherwise.
     """
-    if len(L) < i:
-        return None
-    else:
+    
+    try:
         return L[i]
+    except IndexError:
+        print "This value does not exist!!"
+        return None
 
 # 2
 # Modify to use try-except to return the sum of all numbers in L,
@@ -18,12 +20,14 @@ def sum_of_numbers(L):
     """
     Return the sum of all the numbers in L.
     """
+
     s = 0
     for l in L:
-        if type(l) is int or type(l) is float:
+        try:
             s += l
+        except TypeError:
+            pass
     return s
 
 # TEST
-# sum_of_numbers([3, 1.9, 's']) == 4.9
-
+#sum_of_numbers([3, 1.9, 's']) == 4.9
